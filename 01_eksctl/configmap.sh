0b72bc4a708fa168c548092b22141651e77f256a
Kelly <kellykjd@gmail.com>
2024-07-18 15:23:36 -0300

    Add path to kubectl

--- 01_eksctl/configmap.sh
+++ 01_eksctl/configmap.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-#1- Map user
-#echo "Mapping $AWS_USER user"
+# Variables
+KUBECTL_PATH=/home/ubuntu/bin/kubectl
 
-#kubectl edit -n kube-system configmap/aws-auth
+1- Map user
+echo "Mapping $AWS_USER user"
 
-#mapUsers: |
-#	- groups:
-#		- system:masters
-#		userarn: $AWS_USER
-#		username: $ARN_AWS_USER
+$KUBECTL_PATH edit -n kube-system configmap/aws-auth
+
+mapUsers: |
+	- groups:
+		- system:masters
+		userarn: $AWS_USER
+		username: $ARN_AWS_USER
       
 #2- Get the ConfigMap file
 echo "Get the ConfigMap file"
 kubectl describe configmap aws-auth -n kube-system
 
+#3- Get the cluster info
+kubectl get nodes
