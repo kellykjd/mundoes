7b59ff2d5293a69ce85c42e024a3d82fbc386fbe
Kelly <kellykjd@gmail.com>
2024-07-18 15:57:15 -0300

    Add EOF

--- 01_eksctl/configmap.sh
+++ 01_eksctl/configmap.sh
@@ -5,12 +5,12 @@ KUBECTL_PATH=/home/ubuntu/bin/kubectl
 
 #1- Get the ConfigMap file
 echo "Get the ConfigMap file"
-kubectl describe configmap aws-auth -n kube-system
+KUBECTL_PATH describe configmap aws-auth -n kube-system
 
 #2- Checking user access to the cluster
 echo "Checking user access to the cluster"
 
-kubectl get nodes
+KUBECTL_PATH get nodes
 if [ $? -eq 0 ]
 then
   echo "El usuario está habilitado para realizar consultas al cluster."
