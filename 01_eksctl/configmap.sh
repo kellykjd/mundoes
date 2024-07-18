ca0b3ac309fe53cff041c655161eb5d6c7fd5f6f
Kelly Jimenez <36087642+kellykjd@users.noreply.github.com>
2024-07-18 10:09:29 -0300

    Update configmap.sh

--- 01_eksctl/configmap.sh
+++ 01_eksctl/configmap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-1- Map user
-echo "Mapping $AWS_USER user"
+#1- Map user
+#echo "Mapping $AWS_USER user"
 
-kubectl edit -n kube-system configmap/aws-auth
+#kubectl edit -n kube-system configmap/aws-auth
 
-mapUsers: |
-	- groups:
-		- system:masters
-		userarn: $AWS_USER
-		username: $ARN_AWS_USER
+#mapUsers: |
+#	- groups:
+#		- system:masters
+#		userarn: $AWS_USER
+#		username: $ARN_AWS_USER
       
-2- Get the ConfigMap file
+#2- Get the ConfigMap file
 echo "Get the ConfigMap file"
 kubectl describe configmap aws-auth -n kube-system
 
