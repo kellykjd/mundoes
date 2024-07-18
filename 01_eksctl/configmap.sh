746ac2a4e7885e23b5fc63489559537555c82dd8
Kelly <kellykjd@gmail.com>
2024-07-17 21:47:12 -0300

    edit user access

--- 01_eksctl/configmap.sh
+++ 01_eksctl/configmap.sh
@@ -2,8 +2,22 @@
 
 kubectl edit -n kube-system configmap/aws-auth
 
-#  mapUsers: |
-#    - groups:
-#    - system:masters
-#      userarn:  <arn:aws:iam::xxxx:user/xxx
-#      username: xxx
+#1- Map user
+echo "Mapping $AWS_USER user"
+mapUsers: |
+	- groups:
+		- system:masters
+		userarn:  $ARN_AWS_USER
+		username: $AWS_USER
+      
+#2- Check config
+echo "Checking configuration..."
+kubectl get configmap -n kube-system
+
+#3- Get the ConfigMap file
+echo "Get the ConfigMap file"
+kubectl describe configmap aws-auth -n kube-system
+
+#4- Get the cluster info
+echo "Get the cluster info"
+kubectl get nodes
