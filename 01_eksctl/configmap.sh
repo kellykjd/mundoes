5750b23b2a27f2b7a523dc443a18acadc090c38a
Kelly Jimenez <36087642+kellykjd@users.noreply.github.com>
2024-07-18 09:10:59 -0300

    Update configmap.sh

--- 01_eksctl/configmap.sh
+++ 01_eksctl/configmap.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-kubectl get nodes
-
+#kubectl get nodes
+echo "Installing kubectl"
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+source ~/.bashrc
+kubectl version --client
 
 
 #1- Map user
