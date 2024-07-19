466fcefc22d92335d193061ac69528c3e36498b8
Kelly <kellykjd@gmail.com>
2024-07-19 00:12:57 -0300

    add install prometheus script

--- 04_monitoreo/prometheus-deploy.sh
+++ 04_monitoreo/prometheus-deploy.sh
@@ -3,38 +3,49 @@
 
 # Variables
 KUBECTL_PATH=/home/ubuntu/bin/kubectl
+CLUSTER_NAME=mundoes-cluster-G7
+AWS_REGION=us-east-1
+SERVICE_ACCOUNT_ROLE_ARN=arn:aws:iam::710500364490:role/driver_EBS_controller_EKS
 
-#Instalación del driver EBS
-echo "Installing EBS drivers"
-$KUBECTL_PATH apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.32"
-
-# Agregar repo de prometheus
-echo "Add prometheus repository"
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
-# Agregar repo de grafana
-echo "Add grafana repository"
-helm repo add grafana https://grafana.github.io/helm-charts
-
-# Actualizar repos
-echo "Update repository"
-helm repo update
-
-# Crear el namespace prometheus
+# Create prometheus namespace
 echo "Create prometheus namespace"
 $KUBECTL_PATH create namespace prometheus
 
-# Desplegar prometheus en EKS
-echo "Deploy prometheus on EKS"
-helm install prometheus prometheus-community/prometheus \
---namespace prometheus \
---set alertmanager.persistentVolume.storageClass="gp2" \
---set server.persistentVolume.storageClass="gp2"
+# Add prometheus repository
+echo "Add prometheus repository"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-# Verificar la instalación de prometheus
-echo "Check prometheus installation"
-$KUBECTL_PATH get all -n prometheus
+# Install Prometheus and Alertmanager with Persistent Volume
+echo "Install Prometheus and Alertmanager with Persistent Volume"
+helm install prometheus prometheus-community/prometheus --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp2"
 
-# Exponer prometheus en la instancia de EC2 en el puerto 8080
-echo "Expose prometheus into the port 8080 on EC2 instance"
-$KUBECTL_PATH port-forward -n prometheus deploy/prometheus-server 8080:9090 --address 0.0.0.0
+helm install alertmanager prometheus-community/alertmanager --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp2"
+
+# Verify Prometheus Pods Status
+echo "Verify Prometheus Pods Status"
+$KUBECTL_PATH get pods --namespace=prometheus
+
+# Create IAM OIDC Provider
+echo "Create IAM OIDC Provider"
+eksctl upgrade cluster --name $CLUSTER_NAME --approve
+eksctl utils associate-iam-oidc-provider --region=$AWS_REGION --cluster=$CLUSTER_NAME --approve
+
+# Check Amazon EBS CSI Driver Addon Versions
+echo "Check Amazon EBS CSI Driver Addon Versions"
+aws eks describe-addon-versions --addon-name aws-ebs-csi-driver
+
+# Install Amazon EBS CSI Driver Addon
+echo "Install Amazon EBS CSI Driver Addon"
+eksctl create addon --name aws-ebs-csi-driver --cluster $CLUSTER_NAME --service-account-role-arn $SERVICE_ACCOUNT_ROLE_ARN --force
+
+# Verify EBS CSI Driver Addon Version
+echo "Verify EBS CSI Driver Addon Version"
+eksctl get addon --name aws-ebs-csi-driver --cluster $CLUSTER_NAME
+
+# Update EBS CSI Driver Addon
+echo "Update EBS CSI Driver Addon"
+eksctl upgrade cluster --name $CLUSTER_NAME --approve
+
+# Verify Prometheus Pods Status Again
+echo "Verify Prometheus Pods Status Again"
+$KUBECTL_PATH get pods --namespace=prometheus
